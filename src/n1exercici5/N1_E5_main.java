c14ccca98299ca12020ad9c05767bd90e9b7c37f
juliatogr <julia.tortosa01@estudiant.upf.edu>
2022-07-14 12:31:31 +0200

    n1e5 finished

---
+++ src/n1exercici5/N1_E5_main.java
@@ -0,0 +1,15 @@
+package n1exercici5;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class N1_E5_main {
+
+	public static void main(String[] args){
+		
+		PiInterface pi = ()->3.1415;
+		System.out.println(pi.getPiValue());
+	}
+
+}
