e3e93560730cd2f8711962589c5edfb23d4915d3
juliatogr <julia.tortosa01@estudiant.upf.edu>
2022-07-14 12:18:05 +0200

    n1e3 finished

---
+++ src/n1exercici3/N1_E3_main.java
@@ -0,0 +1,18 @@
+package n1exercici3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class N1_E3_main {
+
+	public static void main(String[] args){
+		
+		List<String> ls = new ArrayList<String>(
+				Arrays.asList("Gener", "Febrer", "Març", "Abril", "Maig", "Juny", 
+						"Juliol", "Agost", "Setembre", "Octubre", "Novembre", "Desembre"));
+		
+		ls.forEach(s->System.out.println(s));
+	}
+
+}
